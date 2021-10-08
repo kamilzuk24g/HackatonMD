86304fc1b53dc599abe5885e8ecc8c9104cce061
kamilzuk24g <91984812+kamilzuk24g@users.noreply.github.com>
2021-10-08 20:12:31 +0200

    group list

---
+++ src/SmartAdmin.WebUI/Controllers/GroupController.cs
@@ -0,0 +1,30 @@
+﻿namespace SmartAdmin.WebUI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SmartAdmin.WebUI.Data;
+    using SmartAdmin.WebUI.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class GroupController : Controller
+    {
+        private ApplicationDbContext applicationDbContext;
+
+        public GroupController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new GroupsListViewModel()
+            {
+                Groups = this.applicationDbContext.Groups.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
