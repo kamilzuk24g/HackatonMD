feededb95e481232b442b89d36ab7785f6dd2cb1
Piotr Jędryczek <pjedryczek@gmail.com>
2021-10-09 01:15:59 +0200

    Tag has nullable dependencies: DB migration

---
+++ src/SmartAdmin.WebUI/Data/Migrations/20211008231505_TagDependencies.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartAdmin.WebUI.Data.Migrations
+{
+    public partial class TagDependencies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Events_EventId",
+                table: "Tags");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupId",
+                table: "Tags",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventId",
+                table: "Tags",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Events_EventId",
+                table: "Tags",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Events_EventId",
+                table: "Tags");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupId",
+                table: "Tags",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventId",
+                table: "Tags",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Events_EventId",
+                table: "Tags",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
