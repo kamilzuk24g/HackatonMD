fedfd91baf655850deeed3314923936fd4f379b4
droi8d <droi8d@gmail.com>
2021-10-09 00:54:01 +0200

    list update

---
+++ src/SmartAdmin.WebUI/ViewModels/MyEventViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartAdmin.WebUI.ViewModels
+{
+    public class MyEventViewModel
+    {
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string IconPath { get; set; }
+
+        public string Description { get; set; }
+    }
+}
