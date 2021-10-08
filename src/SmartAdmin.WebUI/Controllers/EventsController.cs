62a7f00df77b83ec44097d239b50c4bac0fba862
Robert Kucinski <robert@qsoft.com.pl>
2021-10-09 01:48:45 +0200

    bootstrap js i css plus lista eventów bez db

---
+++ src/SmartAdmin.WebUI/Controllers/EventsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartAdmin.WebUI.Models;
+using System.Threading.Tasks;
+
+namespace SmartAdmin.WebUI.Controllers
+{
+    public class EventsController : Controller
+    {
+        //private readonly IDBService _iDBService;
+        //private readonly IConfiguration Configuration;
+
+        //public EventsController(IDBService context, IConfiguration configuration)
+        //{
+        //    _iDBService = context;
+        //    Configuration = configuration;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult EventsList() => View();
+
+        // GET: NumeryController
+        public async Task<ActionResult> Index(EventsListFilterModel filters)
+        {
+            ViewData.Add("filters", filters);
+            int Paging = 100;
+            //int.TryParse(Configuration["Paging"], out Paging);
+
+            //return View(await _iDBService.EventsGet(
+            //    filters.sortColumn, filters.filtrStatus,
+            //    filters.filtrDataOd, filters.filtrDataDo,
+            //    filters.pageIndex, Paging
+            //    ));
+
+            return View();
+        }
+    }
+}
---
+++ src/SmartAdmin.WebUI/Models/EventsListFilterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartAdmin.WebUI.Models
+{
+    public class EventsListFilterModel
+    {
+        public string sortColumn { get; set; }
+        public int pageIndex { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime? filterDateFrom { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime? filterDateTo { get; set; }
+        public string filterTitle { get; set; }
+        public string filterDesc { get; set; }
+        public string filterPlace { get; set; }
+        public decimal? filterPriceFrom { get; set; }
+        public decimal? filterPriceTo { get; set; }
+        public DateTime? filterTimeFrom { get; set; }
+        public DateTime? filterTimeTo { get; set; }
+        public int filterPeople { get; set; }
+        public string filterTags { get; set; }
+        public string filterCustom { get; set; }
+
+    }
+}
