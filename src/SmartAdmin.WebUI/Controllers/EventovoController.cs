16f1ab7d2c8c281f9555643a6dd619d92a495ea3
kamilzuk24g <91984812+kamilzuk24g@users.noreply.github.com>
2021-10-09 07:26:57 +0200

    events

--- src/SmartAdmin.WebUI/Controllers/EventovoController.cs
+++ src/SmartAdmin.WebUI/Controllers/EventovoController.cs
@@ -4,17 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SmartAdmin.WebUI.ViewModels;
+using SmartAdmin.WebUI.Data;
 
 namespace SmartAdmin.WebUI.Controllers
 {
     public class EventovoController : Controller
     {
+        private ApplicationDbContext applicationDbContext;
+
+        public EventovoController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
         public IActionResult MyEvents()
         {
             var model = new List<MyEventViewModel>()
             {
                 new MyEventViewModel()
                 {
+                    Id = 1,
                      Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -26,6 +35,7 @@ namespace SmartAdmin.WebUI.Controllers
                 },
                 new MyEventViewModel()
                 {
+                    Id = 2,
                      Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -37,6 +47,7 @@ namespace SmartAdmin.WebUI.Controllers
                 },
                 new MyEventViewModel()
                 {
+                    Id = 3,
                      Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -53,5 +64,28 @@ namespace SmartAdmin.WebUI.Controllers
         }
 
         public IActionResult AddEvent() => View();
+
+        public IActionResult Details(int id)
+        {
+            var eventDetails = this.applicationDbContext.Events.FirstOrDefault(x => x.Id == id);
+
+            var eventDates = this.applicationDbContext.ProposedEventDates.Where(x => x.EventId == id).ToList();
+
+            var eventParticipants = this.applicationDbContext.EventParticipants.Where(x => x.EventId == id).ToList();
+
+            var viewModel = new EventDetailsViewModel()
+            {
+                Id = id,
+                Description = eventDetails.EventDescription,
+                FinalEventDate = eventDetails.FinalEventDate,
+                Place = eventDetails.EventPlace,
+                IconPath = @"/img/demo/rails.png",
+                Title = eventDetails.EventName,
+                EstimatedCostPerPerson = eventDetails.EstimatedCostPerPerson.HasValue ? eventDetails.EstimatedCostPerPerson.Value.ToString("N2") + "PLN" : "",
+                ProposedEventDates = eventDates.Select(x => x.ProposedDate).ToList(),
+            };
+
+            return View(viewModel);
+        }
     }
 }
